9424b90b54c096fd7b78f49b2fa66cec6eeb951f
bmehder <bmehder@gmail.com>
2022-04-12 22:20:31 +0200

    added anchor prop to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import Youtube from './Youtube.svelte'
 
-export default function (target, props) {
+export default function (target, props, anchor = null) {
   new Youtube({
     target,
     props,
+    anchor,
   })
 }
